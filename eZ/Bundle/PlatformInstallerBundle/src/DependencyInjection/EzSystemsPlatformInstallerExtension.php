1399eaa8f5d672b9695edd33530e6344268405bc
Bertrand Dunogier <bd@ez.no>
2015-02-03 16:03:01 +0100

    EZP-23949: Fixed default SA in config

--- eZ/Bundle/PlatformInstallerBundle/src/DependencyInjection/EzSystemsPlatformInstallerExtension.php
+++ eZ/Bundle/PlatformInstallerBundle/src/DependencyInjection/EzSystemsPlatformInstallerExtension.php
@@ -14,7 +14,6 @@ class EzSystemsPlatformInstallerExtension extends Extension
     public function load( array $configs, ContainerBuilder $container )
     {
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
-        echo "Loading services.yml\n";
         $loader->load( 'services.yml' );
     }
 }
