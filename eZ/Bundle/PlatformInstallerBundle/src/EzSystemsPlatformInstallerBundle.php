b5eacad2d00311e12e1ddf6d5c46416f4f6acd82
Bertrand Dunogier <bd@ez.no>
2015-02-03 17:21:58 +0100

    Moved files for merge into kernel

---
+++ eZ/Bundle/PlatformInstallerBundle/src/EzSystemsPlatformInstallerBundle.php
@@ -0,0 +1,9 @@
+<?php
+namespace EzSystems\PlatformInstallerBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzSystemsPlatformInstallerBundle extends Bundle
+{
+}
